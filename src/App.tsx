2fed87d52656eed69cf71e00e63f2ad1ecc6acce
JaroslawX <ad.jaro@wp.pl>
2024-10-29 09:14:49 +0100

    Data Flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import './App.css';
 import BookList from "./Components/BookList/BookList";
 import Search from "./Components/Search/Search";
+import {ChangeEvent, SyntheticEvent, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-        <Search />
-        <BookList />
-    </div>
+    const [search, setSearch] = useState<string>("")
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target.value);
+        console.log(e);
+    };
+
+    const onClick = (e: SyntheticEvent) => {
+        console.log(e);
+    };
+    
+    return (
+        <div className="App">
+            <Search onClick={onClick} search={search} handleChange={handleChange} />
+            <BookList />
+        </div>
   );
 }
 
