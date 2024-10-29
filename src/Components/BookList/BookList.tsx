ce34339a66122d960485aecb90edd7851e4d6792
JaroslawX <ad.jaro@wp.pl>
2024-10-29 09:26:23 +0100

    test

--- src/Components/BookList/BookList.tsx
+++ src/Components/BookList/BookList.tsx
@@ -11,6 +11,7 @@ const BookList : React.FC<Props> = (props: Props): JSX.Element => {
             <Book title="The Bible" author="God and human" description="book"/>
             <Book title="The Chronicles of Narnia" author="C.S. Lewis" description="book"/>
             <Book title="Harry Potter" author="J.K. Rowling" description="book"/>
+            <Book title="1984" author="George Orwell" description="book" />
         </div>
     );
 };
