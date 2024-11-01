f0c8a0f383784fe036c201e3dbd01961854b031f
JaroslawX <ad.jaro@wp.pl>
2024-11-01 10:50:40 +0100

    create navbar

---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Navbar.css";
+
+interface Props {}
+
+const Navbar: React.FC<Props> = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-logo">Library</div>
+            <ul className="navbar-links">
+                <li><a href="/">Home</a></li>
+                <li><a href="/books">Books</a></li>
+                <li><a href="/bookshelves">Bookshelves</a></li>
+                <li><a href="/about">About</a></li>
+            </ul>
+            <div className="navbar-login">
+                <button>Login</button>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
