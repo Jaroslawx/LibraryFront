0e10566ee5ac140f919c4ed37c02f25a0c7d9aad
JaroslawX <ad.jaro@wp.pl>
2024-11-02 09:29:48 +0100

    navbar tailwindcss

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -5,18 +5,26 @@ interface Props {}
 
 const Navbar: React.FC<Props> = () => {
     return (
-        <nav className="navbar">
-            <div className="navbar-logo">BookNest</div>
-            <ul className="navbar-links">
-                <li><a href="/">Home</a></li>
-                <li><a href="/books">Books</a></li>
-                <li><a href="/bookshelves">Bookshelves</a></li>
-                <li><a href="/about">About</a></li>
-            </ul>
-            <div className="navbar-login">
-                <button>Login</button>
+        <nav className="flex justify-between items-center bg-gray-800 p-4 text-white">
+            <div className="flex items-center space-x-3">
+                <img
+                    src="https://res.cloudinary.com/ggeztrw22/image/upload/v1730534937/Library/aferpbynnxe5o9bepwps.png"
+                    alt="BookNest logo"
+                    className="w-20 h-20"
+                />
+                <span className="text-2xl font-bold">BookNest</span>
             </div>
+            <ul className="flex space-x-6">
+                <li><a href="/" className="hover:text-yellow-500">Home</a></li>
+                <li><a href="/books" className="hover:text-yellow-500">Books</a></li>
+                <li><a href="/bookshelves" className="hover:text-yellow-500">Bookshelves</a></li>
+                <li><a href="/about" className="hover:text-yellow-500">About</a></li>
+            </ul>
+            <button className="bg-yellow-500 hover:bg-yellow-600 text-white py-2 px-4 rounded">
+                Login
+            </button>
         </nav>
+
     );
 };
 
