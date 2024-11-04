544b85b41ccd58bcb38f2f166233567eda4d4c86
JaroslawX <ad.jaro@wp.pl>
2024-11-04 12:20:54 +0100

    book details

--- src/library.d.ts
+++ src/library.d.ts
@@ -13,13 +13,37 @@ export interface Book {
     title: string;                 // Book title
     author_name?: string[];        // List of authors of the book
     author_key?: string[];         // Keys of authors in Open Library
+    cover?: Cover;                 // Cover image
+    subjects?: string[];           // Subjects related to the book
+    description?: string;          // Description of the book
+}
+
+export interface Cover {
+    key: string;                 // Cover key
+    small: string;               // Link to small cover image
+    medium: string;              // Link to medium cover image
+    large: string;               // Link to large cover image
+}
+
+export interface BookDetails {
+    key: string;                   // Book key in Open Library
+    title: string;                 // Book title
+    author_name?: string[];        // List of authors of the book
+    author_key?: string[];         // Keys of authors in Open Library
     publish_year?: number[];       // Year of publication
     first_publish_year?: number;   // Year of first publication
     isbn?: string[];               // List of ISBNs
     number_of_pages_median?: number; // Median number of pages
     cover?: Cover;                 // Cover image
     subjects?: string[];           // Subjects related to the book
-    description?: string;          // Description of the book
+    description?: string;     // Subjects related to the book
+    publishers?: string[];           // Publishers of the book
+    publish_places?: string[];       // Places of publication
+    languages?: { key: string }[];   // Languages the book is available in
+    number_of_pages?: number;        // Total number of pages
+    created?: { type: string, value: string }; // Creation timestamp
+    last_modified?: { type: string, value: string }; // Last modification timestamp
+    cover_edition_key?: string;      // Key for the cover edition
 }
 
 export interface Author {
@@ -29,13 +53,6 @@ export interface Author {
     death_date?: string;         // Death date
     top_work?: string;           // Author's top work
     works_count?: number;        // Number of works by the author
-}
-
-export interface Cover {
-    key: string;                 // Cover key
-    small: string;               // Link to small cover image
-    medium: string;              // Link to medium cover image
-    large: string;               // Link to large cover image
 }
 
 
