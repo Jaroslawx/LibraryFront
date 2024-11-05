1559f2258253e3fea36401c89d80f5247a9cb347
JaroslawX <ad.jaro@wp.pl>
2024-11-05 09:49:24 +0100

    new function and update

--- src/api.tsx
+++ src/api.tsx
@@ -8,7 +8,7 @@ interface SearchResponse {
 export const searchBooks = async (query: string): Promise<Book[] | string>=> {
     try {
         const response= await axios.get<SearchResponse>(
-            `https://openlibrary.org/search.json?q=${query}&apikey=${process.env.REACT_APP_OPEN_LIBRARY_API_KEY}`
+            `https://openlibrary.org/search.json?q=${encodeURIComponent(query)}`
         );
         return response.data.docs;
     } 
@@ -24,20 +24,38 @@ export const searchBooks = async (query: string): Promise<Book[] | string>=> {
     } 
 }
 
-export const getBookDetails = async (query: string) => {
+export const getBookDetails = async (olid: string): Promise<BookDetails | string> => {
     try {
         const response = await axios.get<{ [key: string]: BookDetails }>(
-            `https://openlibrary.org/api/books?bibkeys=OLID:${query}&format=json&jscmd=data`
+            `https://openlibrary.org/api/books?bibkeys=OLID:${olid}&format=json&jscmd=data`
         );
-        const data = response.data[query]; // Access the specific book data using the key
-        return data ? data : "Book not found!";
+        
+        const bookData = response.data[`OLID:${olid}`];
+        return bookData || "Book not found!";
     } catch (error: any) {
         if (axios.isAxiosError(error)) {
-            console.log("error message from API: ", error.message);
-            return error.message;
+            console.log("Error message from API: ", error.message);
+            throw new Error(error.message);
         } else {
             console.log("unexpected error: ", error);
-            return "An unexpected error occurred.";
+            throw new Error("An unexpected error occurred.");
+        }
+    }
+}
+
+export const getWorkDetails = async (workId: string): Promise<BookDetails | string> => {
+    try {
+        const response = await axios.get<BookDetails>(
+            `https://openlibrary.org/works/${workId}.json`
+        );
+        return response.data;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            console.error("Error message from API:", error.message);
+            throw new Error(error.message);
+        } else {
+            console.error("Unexpected error:", error);
+            throw new Error("An unexpected error occurred.");
         }
     }
 }
