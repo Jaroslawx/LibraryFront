f724fcd86cc2b49b8c49369c0d0664fdc019e85c
JaroslawX <ad.jaro@wp.pl>
2024-11-06 17:24:43 +0100

    interface update

--- src/library.d.ts
+++ src/library.d.ts
@@ -1,48 +1,30 @@
 // library.d.ts
-
-export interface BookSearch {
-    numFound: number;              // Number of books found
-    start: number;                 // Starting position in the results
-    docs: Book[];                  // List of books
-    nextPage?: number;             // Optional number of the next page of results
-    previousPage?: number;         // Optional number of the previous page of results
-}
-
 export interface Book {
-    key: string;                   // Book key in Open Library
-    title: string;                 // Book title
-    author_name?: string[];        // List of authors of the book
-    author_key?: string[];         // Keys of authors in Open Library
-    cover?: Cover;                 // Cover image
-    subjects?: string[];           // Subjects related to the book
-    description?: string;          // Description of the book
-}
-
-export interface Cover {
-    key: string;                 // Cover key
-    small: string;               // Link to small cover image
-    medium: string;              // Link to medium cover image
-    large: string;               // Link to large cover image
+    key: string;
+    title: string;
+    author_name?: string[];
+    author_key?: string[];
+    cover?: Cover;
+    subjects?: string[];
+    description?: string;
 }
 
 interface BookDetails {
+    key: string;
     title: string;
-    authors: { name: string }[];
-    publish_date: string;
+    authors?: { key: string }[];
     subjects: string[];
     description?: string | { value: string };
     cover?: { large: string };
 }
 
 export interface Author {
-    key: string;                 // Author key in Open Library
-    name: string;                // Author name
-    birth_date?: string;         // Birthdate
-    death_date?: string;         // Death date
-    top_work?: string;           // Author's top work
-    works_count?: number;        // Number of works by the author
+    personal_name: string;
 }
 
-
-
-
+export interface Cover {
+    key: string;
+    small: string;
+    medium: string;
+    large: string;
+}
