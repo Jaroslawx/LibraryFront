0918c3f1c1be12d2d30cfe925fa23a1277a79a4b
JaroslawX <ad.jaro@wp.pl>
2024-11-07 09:14:22 +0100

    refactor and css

--- src/Components/BookList/BookList.tsx
+++ src/Components/BookList/BookList.tsx
@@ -1,11 +1,11 @@
 import React, {SyntheticEvent} from "react";
 import "./BookList.css";
 import Book from "../../Components/Book/Book";
-import { Book as BookType } from "../../library";
+import { BookSearch } from "../../library";
 import { v4 as uuidv4 } from "uuid";
 
 interface Props {
-    searchResults: BookType[];
+    searchResults: BookSearch[];
     onBookshelfCreate: (e: SyntheticEvent) => void;
 }
 
@@ -13,16 +13,16 @@ const BookList: React.FC<Props> = ({ searchResults, onBookshelfCreate }: Props):
     return (
         <>
             {searchResults.length > 0 ? (
-                searchResults.map((result) => {
-                    return (
+                <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                    {searchResults.map((result) => (
                         <Book
                             key={uuidv4()}
                             id={result.key}
                             searchResult={result}
                             onBookshelfCreate={onBookshelfCreate}
                         />
-                    );
-                })
+                    ))}
+                </div>
             ) : (
                 <p className="mb-3 mt-3 text-xl font-semibold text-center md:text-xl">
                     No results found!
