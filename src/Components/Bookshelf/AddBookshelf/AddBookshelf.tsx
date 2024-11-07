af17723b7a364c3332b2e65398c378464fb0e79c
JaroslawX <ad.jaro@wp.pl>
2024-11-07 10:19:47 +0100

    Add to bookshelf button css

--- src/Components/Bookshelf/AddBookshelf/AddBookshelf.tsx
+++ src/Components/Bookshelf/AddBookshelf/AddBookshelf.tsx
@@ -6,10 +6,17 @@ interface Props {
 }
 
 const AddBookshelf = ({onBookshelfCreate, title}: Props) => {
-  return <form onSubmit={onBookshelfCreate}>
-    <input readOnly={true} hidden={true} value={title}/>
-    <button type="submit">Add to Bookshelf</button>
-  </form>;
+  return (
+      <form onSubmit={onBookshelfCreate}>
+        <input readOnly={true} hidden={true} value={title} />
+        <button
+            type="submit"
+            className="px-6 py-2 mt-4 bg-blue-500 text-white font-semibold rounded-lg shadow-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 transition-colors"
+        >
+          Add to Bookshelf
+        </button>
+      </form>
+  );
 };
 
 export default AddBookshelf;
