2beed0eb10be16782a462e44c7bf99e6d47ca353
JaroslawX <ad.jaro@wp.pl>
2024-11-08 10:41:35 +0100

    design routes

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -3,6 +3,7 @@ import App from "../App";
 import HomePage from "../Pages/HomePage/HomePage";
 import SearchPage from "../Pages/SearchPage/SearchPage";
 import BookPage from "../Pages/BookPage/BookPage";
+import DesignPage from "../Pages/DesignPage/DesignPage";
 
 export const router = createBrowserRouter([
     {
@@ -11,6 +12,7 @@ export const router = createBrowserRouter([
         children: [
             {path: "", element: <HomePage /> },
             {path: "search", element: <SearchPage /> },
+            {path: "design-guide", element: <DesignPage /> },
             {path: "book/:bookId", element: <BookPage /> },
         ]
     }
