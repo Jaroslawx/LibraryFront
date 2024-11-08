c6a2ecf1a14906de4a5b1c6077c739678896f745
JaroslawX <ad.jaro@wp.pl>
2024-11-08 10:41:47 +0100

    table testData

---
+++ src/Components/Table/testData.tsx
@@ -0,0 +1,14 @@
+export const testBookData = [
+    {
+        title: "The Hobbit",
+        author: "J.R.R. Tolkien",
+        genre: "Fantasy",
+        year: 1937
+    },
+    {
+        title: "The Fellowship of the Ring",
+        author: "J.R.R. Tolkien",
+        genre: "Fantasy",
+        year: 1954
+    }
+]
