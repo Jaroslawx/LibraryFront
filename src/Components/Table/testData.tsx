616777dcea9bdaba061e7a77392dcdacee0b7485
JaroslawX <ad.jaro@wp.pl>
2024-11-08 10:51:00 +0100

    more data

--- src/Components/Table/testData.tsx
+++ src/Components/Table/testData.tsx
@@ -1,14 +1,62 @@
 export const testBookData = [
     {
-        title: "The Hobbit",
-        author: "J.R.R. Tolkien",
-        genre: "Fantasy",
-        year: 1937
+        title: "To Kill a Mockingbird",
+        author_name: "Harper Lee",
+        subjects: "Fiction, Social Injustice",
+        publish_year: 1960,
     },
     {
-        title: "The Fellowship of the Ring",
-        author: "J.R.R. Tolkien",
-        genre: "Fantasy",
-        year: 1954
+        title: "1984",
+        author_name: "George Orwell",
+        subjects: "Dystopian, Political Fiction",
+        publish_year: 1949,
+    },
+    {
+        title: "Pride and Prejudice",
+        author_name: "Jane Austen",
+        subjects: "Romance, Social Criticism",
+        publish_year: 1813,
+    },
+    {
+        title: "The Great Gatsby",
+        author_name: "F. Scott Fitzgerald",
+        subjects: "Tragedy, American Dream",
+        publish_year: 1925,
+    },
+    {
+        title: "Moby-Dick",
+        author_name: "Herman Melville",
+        subjects: "Adventure, Sea Story",
+        publish_year: 1851,
+    },
+    {
+        title: "War and Peace",
+        author_name: "Leo Tolstoy",
+        subjects: "Historical Fiction, Russian Literature",
+        publish_year: 1869,
+    },
+    {
+        title: "The Catcher in the Rye",
+        author_name: "J.D. Salinger",
+        subjects: "Coming of Age, Realism",
+        publish_year: 1951,
+    },
+    {
+        title: "The Odyssey",
+        author_name: "Homer",
+        subjects: "Epic, Mythology",
+        publish_year: -800,
+    },
+    {
+        title: "Crime and Punishment",
+        author_name: "Fyodor Dostoevsky",
+        subjects: "Psychological Fiction, Crime",
+        publish_year: 1866,
+    },
+    {
+        title: "Brave New World",
+        author_name: "Aldous Huxley",
+        subjects: "Science Fiction, Dystopian",
+        publish_year: 1932,
     }
-]
+];
